57f2afa121f56273cc7e41cecb39a26ce08f1312
leví <lewisvicna@gmail.com>
2022-10-17 14:14:04 +0200

    arreglo en metodos de searchtesty

--- SearchTest/SearchTest.cs
+++ SearchTest/SearchTest.cs
@@ -8,49 +8,46 @@ namespace SearchTest
     [TestClass]
     public class SearchTest
     {
+
         [TestMethod]
-        public void SearchControllerTest()
+        public void GetAsyncReturnsOK()
         {
-            [TestMethod]
-            void GetAsyncReturnsOK()
-            {
-                IEstanteriaService estanteriaService = new EstanteriasFake();
-                IProductoService productoService = new ProductosFake();
-                IAlbaranService albaranService = new AlbaranFake();
+            IEstanteriaService estanteriaService = new EstanteriasFake();
+            IProductoService productoService = new ProductosFake();
+            IAlbaranService albaranService = new AlbaranFake();
 
-                var searchController = new SearchController(
-                    estanteriaService,
-                    productoService,
-                    albaranService);
+            var searchController = new SearchController(
+                estanteriaService,
+                productoService,
+                albaranService);
 
-                var resultCaducidad = searchController.CaducidadAsync();
-                var resultAlbaran = searchController.AlbaranAsync("1");
-                Assert.IsNotNull(resultCaducidad);
-                Assert.IsNotNull(resultAlbaran);
-                Assert.IsInstanceOfType(resultCaducidad, typeof(OkObjectResult));
-                Assert.IsInstanceOfType(resultAlbaran, typeof(OkObjectResult));
-            }
+            var resultCaducidad = searchController.CaducidadAsync();
+            var resultAlbaran = searchController.AlbaranAsync("1");
+            Assert.IsNotNull(resultCaducidad);
+            Assert.IsNotNull(resultAlbaran);
+            Assert.IsInstanceOfType(resultCaducidad, typeof(OkObjectResult));
+            Assert.IsInstanceOfType(resultAlbaran, typeof(OkObjectResult));
+        }
 
-            [TestMethod]
-            void GetAsyncReturnsNotOK()
-            {
-                IEstanteriaService estanteriaService = new EstanteriasFake();
-                IProductoService productoService = new ProductosFake();
-                IAlbaranService albaranService = new AlbaranFake();
+        [TestMethod]
+        public void GetAsyncReturnsNotOK()
+        {
+            IEstanteriaService estanteriaService = new EstanteriasFake();
+            IProductoService productoService = new ProductosFake();
+            IAlbaranService albaranService = new AlbaranFake();
 
-                var searchController = new SearchController(
-                    estanteriaService,
-                    productoService,
-                    albaranService);
+            var searchController = new SearchController(
+                estanteriaService,
+                productoService,
+                albaranService);
 
-                var resultAlbaran = searchController.AlbaranAsync("666");
-                var resultCaducidad = searchController.CaducidadAsync();
+            var resultAlbaran = searchController.AlbaranAsync("666");
+            var resultCaducidad = searchController.CaducidadAsync();
 
-                Assert.IsNotNull(resultCaducidad);
-                Assert.IsNotNull(resultAlbaran);
-                Assert.IsInstanceOfType(resultCaducidad, typeof(NotFoundResult));
-                Assert.IsInstanceOfType(resultAlbaran, typeof(NotFoundResult));
-            }
+            Assert.IsNotNull(resultCaducidad);
+            Assert.IsNotNull(resultAlbaran);
+            Assert.IsInstanceOfType(resultCaducidad, typeof(NotFoundResult));
+            Assert.IsInstanceOfType(resultAlbaran, typeof(NotFoundResult));
         }
     }
 }
