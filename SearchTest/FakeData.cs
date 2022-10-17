f33db8a9496cda4b67fecbfb5a188370014f0a25
leví <lewisvicna@gmail.com>
2022-10-17 14:03:29 +0200

    primera versión de pruebas de search

---
+++ SearchTest/FakeData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GrupoC.Search.Interfaces;
+using GrupoC.Search.Models;
+
+namespace SearchTest
+{
+
+    class ProductosFake : IProductoService
+    {
+        public static List<Producto> productos = new()
+            {
+                new Producto()
+                {
+                    Id = "1",
+                    Nombre = "Caca",
+                    Caducidad = new DateTime(2022, 10, 1)
+                },
+                new Producto()
+                {
+                    Id = "2",
+                    Nombre = "Caca2",
+                    Caducidad = new DateTime(2022, 10, 28)
+                }
+            };
+
+        public Task<Producto?> GetAsync(string id)
+        {
+            var producto = productos.FirstOrDefault(x => x.Id.Equals(id));
+            return Task.FromResult(producto);
+        }
+
+        public Task<List<Producto>> GetAllAsync()
+        {
+            return Task.FromResult(productos);
+        }
+    }
+
+    class EstanteriasFake : IEstanteriaService
+    {
+        public Task<Estanteria?> GetAsync(string estanteriaId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class AlbaranFake : IAlbaranService
+    {
+        private List<Albaran> albaranes = new()
+        {
+            new Albaran()
+            {
+                Id = "1",
+                AlbaranDate = DateTime.Now
+            }
+        };
+        public Task<ICollection<Albaran>> GetAsync(string estanteriaId)
+        {
+            ICollection<Albaran> coleccion = albaranes;
+            return Task.FromResult(coleccion);
+        }
+    }
+}
+
---
+++ SearchTest/SearchTest.cs
@@ -0,0 +1,56 @@
+using GrupoC.Search.Controllers;
+using GrupoC.Search.Interfaces;
+using GrupoC.Search.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchTest
+{
+    [TestClass]
+    public class SearchTest
+    {
+        [TestMethod]
+        public void SearchControllerTest()
+        {
+            [TestMethod]
+            void GetAsyncReturnsOK()
+            {
+                IEstanteriaService estanteriaService = new EstanteriasFake();
+                IProductoService productoService = new ProductosFake();
+                IAlbaranService albaranService = new AlbaranFake();
+
+                var searchController = new SearchController(
+                    estanteriaService,
+                    productoService,
+                    albaranService);
+
+                var resultCaducidad = searchController.CaducidadAsync();
+                var resultAlbaran = searchController.AlbaranAsync("1");
+                Assert.IsNotNull(resultCaducidad);
+                Assert.IsNotNull(resultAlbaran);
+                Assert.IsInstanceOfType(resultCaducidad, typeof(OkObjectResult));
+                Assert.IsInstanceOfType(resultAlbaran, typeof(OkObjectResult));
+            }
+
+            [TestMethod]
+            void GetAsyncReturnsNotOK()
+            {
+                IEstanteriaService estanteriaService = new EstanteriasFake();
+                IProductoService productoService = new ProductosFake();
+                IAlbaranService albaranService = new AlbaranFake();
+
+                var searchController = new SearchController(
+                    estanteriaService,
+                    productoService,
+                    albaranService);
+
+                var resultAlbaran = searchController.AlbaranAsync("666");
+                var resultCaducidad = searchController.CaducidadAsync();
+
+                Assert.IsNotNull(resultCaducidad);
+                Assert.IsNotNull(resultAlbaran);
+                Assert.IsInstanceOfType(resultCaducidad, typeof(NotFoundResult));
+                Assert.IsInstanceOfType(resultAlbaran, typeof(NotFoundResult));
+            }
+        }
+    }
+}
