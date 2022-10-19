55d1d1e63808a4a09b3544ca2f6527bef915d487
victor9889 <victormusik98@hotmail.com>
2022-10-19 13:14:54 +0200

    Secretos cadena conexión estanterias

--- GrupoC.Estanteria/Program.cs
+++ GrupoC.Estanteria/Program.cs
@@ -1,14 +1,39 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 using GrupoC.Estanteria.DAL;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+    {
+        Delay= TimeSpan.FromSeconds(2),
+        MaxDelay = TimeSpan.FromSeconds(16),
+        MaxRetries = 5,
+        Mode = RetryMode.Exponential
+    }
+};
+var client = new SecretClient(new Uri("https://clavesgrupoc.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+KeyVaultSecret secret = client.GetSecret("ConnectionStringEstanterias");
+
+string secretValue = secret.Value;
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEstanteriaProvider, EstanteriaProvider>();
+builder.Services.AddDbContext<EstanteriaContext>(options =>
+                    options.UseSqlServer(secretValue)
+);
+
 
 var app = builder.Build();
 
