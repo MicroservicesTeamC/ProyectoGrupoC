77a6d7b2c0f1954f6cf233803579c7011bc63664
leví <lewisvicna@gmail.com>
2022-10-26 09:09:29 +0200

    Primer comit a rama sin key secrets

--- GrupoC.Estanteria/Program.cs
+++ GrupoC.Estanteria/Program.cs
@@ -8,25 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-SecretClientOptions options = new SecretClientOptions()
-{
-    Retry =
-    {
-        Delay= TimeSpan.FromSeconds(2),
-        MaxDelay = TimeSpan.FromSeconds(16),
-        MaxRetries = 5,
-        Mode = RetryMode.Exponential
-    }
-};
+//SecretClientOptions options = new SecretClientOptions()
+//{
+//    Retry =
+//    {
+//        Delay= TimeSpan.FromSeconds(2),
+//        MaxDelay = TimeSpan.FromSeconds(16),
+//        MaxRetries = 5,
+//        Mode = RetryMode.Exponential
+//    }
+//};
 
-string secretValue = "";
-string? Uri = Environment.GetEnvironmentVariable("KEY_VAULT_URI", EnvironmentVariableTarget.User);
-if(Uri is not null)
-{
-    var client = new SecretClient(new Uri(Uri), new DefaultAzureCredential(), options);
-    KeyVaultSecret secret = client.GetSecret(Environment.GetEnvironmentVariable("SECRET_ESTANTERIA", EnvironmentVariableTarget.User));
-    secretValue = secret.Value;
-}
+//string secretValue = "";
+//string? Uri = Environment.GetEnvironmentVariable("KEY_VAULT_URI", EnvironmentVariableTarget.User);
+//if(Uri is not null)
+//{
+//    var client = new SecretClient(new Uri(Uri), new DefaultAzureCredential(), options);
+//    KeyVaultSecret secret = client.GetSecret(Environment.GetEnvironmentVariable("SECRET_ESTANTERIA", EnvironmentVariableTarget.User));
+//    secretValue = secret.Value;
+//}
 
 
 
@@ -37,7 +37,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IEstanteriaProvider, EstanteriaProvider>();
 builder.Services.AddDbContext<EstanteriaContext>(options =>
-                    options.UseSqlServer(secretValue)
+                    options.UseSqlServer("Server=tcp:productosgrupoc.database.windows.net,1433;Initial Catalog=Estanterias;Persist Security Info=False;User ID=admingrupoc;Password=Grupocadmin00;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
 );
 
 
--- ProyectoGrupoC/Program.cs
+++ ProyectoGrupoC/Program.cs
@@ -7,25 +7,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-SecretClientOptions options = new SecretClientOptions()
-{
-    Retry =
-    {
-        Delay= TimeSpan.FromSeconds(2),
-        MaxDelay = TimeSpan.FromSeconds(16),
-        MaxRetries = 5,
-        Mode = RetryMode.Exponential
-    }
-};
+//SecretClientOptions options = new SecretClientOptions()
+//{
+//    Retry =
+//    {
+//        Delay= TimeSpan.FromSeconds(2),
+//        MaxDelay = TimeSpan.FromSeconds(16),
+//        MaxRetries = 5,
+//        Mode = RetryMode.Exponential
+//    }
+//};
 
-string secretValue = "";
-string? Uri = Environment.GetEnvironmentVariable("KEY_VAULT_URI", EnvironmentVariableTarget.User);
-if(Uri is not null)
-{
-    var client = new SecretClient(new Uri(Uri), new DefaultAzureCredential(), options);
-    KeyVaultSecret secret = client.GetSecret(Environment.GetEnvironmentVariable("SECRET_ALBARAN", EnvironmentVariableTarget.User));
-    secretValue = secret.Value;
-}
+//string secretValue = "";
+//string? Uri = Environment.GetEnvironmentVariable("KEY_VAULT_URI", EnvironmentVariableTarget.User);
+//if(Uri is not null)
+//{
+//    var client = new SecretClient(new Uri(Uri), new DefaultAzureCredential(), options);
+//    KeyVaultSecret secret = client.GetSecret(Environment.GetEnvironmentVariable("SECRET_ALBARAN", EnvironmentVariableTarget.User));
+//    secretValue = secret.Value;
+//}
 
 // Add services to the container.
 
@@ -35,7 +35,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAlbaranProvider, AlbaranProvider>();
 builder.Services.AddDbContext<AlbaranContext>(options =>
-                    options.UseSqlServer(secretValue)
+                    options.UseSqlServer("Server=tcp:productosgrupoc.database.windows.net,1433;Initial Catalog=Albaranes;Persist Security Info=False;User ID=admingrupoc;Password=Grupocadmin00;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")
 );
 
 var app = builder.Build();
